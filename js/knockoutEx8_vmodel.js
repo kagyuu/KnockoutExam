8959c9abc2898de2f35632e56be54ede2c200879
HONDOH Atsushi <kagyuu@hondou.homedns.org>
2015-03-10 01:58:33 +0900

    Add Empty section to Ex8

--- js/knockoutEx8_vmodel.js
+++ js/knockoutEx8_vmodel.js
@@ -3,17 +3,28 @@
  */
 define(['knockout'], function(ko) {
 
-	return function appViewModel() {
-		var self = this;
-		self.msgClick = ko.observable("");
-		self.divClick = function (data, event) {
-			self.msgClick("div " + (event.shiftKey ? "+Shift" : ""));
-		};
-		self.aClick = function (data, event) {
-			self.msgClick("link " + (event.shiftKey ? "+Shift" : ""));
-		};
-		self.btnClick = function (data, event) {
-			self.msgClick("button " + (event.shiftKey ? "+Shift" : ""));
-		};
-	};
+  return function appViewModel() {
+    var self = this;
+    
+    <!-- Event Binding Example -->
+    self.msgClick = ko.observable("");
+    self.divClick = function (data, event) {
+      self.msgClick("div " + (event.shiftKey ? "+Shift" : ""));
+    };
+    self.aClick = function (data, event) {
+      self.msgClick("link " + (event.shiftKey ? "+Shift" : ""));
+    };
+    self.btnClick = function (data, event) {
+      self.msgClick("button " + (event.shiftKey ? "+Shift" : ""));
+    };
+    
+    <!-- Submit Binding Example -->
+    <!-- Enable/disable Binding Example -->
+    <!-- Value Binding Example -->
+    <!-- Hasfocus Binding Example -->
+    <!-- Checked Binding Example -->
+    <!-- Options Binding Example -->
+    <!-- SelectedOptions Binding Example -->
+    <!-- UniqueNmae Binding Example -->
+  };
 });
